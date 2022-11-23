0311e3a1b975f04ebda54afdd536097406a2f46a
sankdeveloper <49100301+sankdeveloper@users.noreply.github.com>
2022-11-23 12:56:16 +0530

    Create RangeLoopTest.cs

---
+++ ShortSharp/test/Test.ShortSharp/RangeLoopTest.cs
@@ -0,0 +1,73 @@
+using FluentAssertions;
+using ShortSharp.Extensions;
+
+namespace Test.ShortSharp;
+
+public class RangeLoopTest
+{
+    [Fact]
+    public void CanRunFromToRange()
+    {
+        //Arrange
+        List<int> result = new();
+        
+        //Act
+        foreach (var i in 0..10)
+        {
+            result.Add(i);
+        }
+
+        //Assert
+        for (var index = 0; index < result.Count; index++)
+        {
+            var number = index;
+            result.Should().HaveElementAt(index, number);
+        }
+
+        result.Should().HaveCount(11);
+    }
+    
+    [Fact]
+    public void CanRunUptoRange()
+    {
+        //Arrange
+        List<int> result = new();
+        
+        //Act
+        foreach (var i in ..10)
+        {
+            result.Add(i);
+        }
+
+        //Assert
+        for (var index = 0; index < result.Count; index++)
+        {
+            var number = index;
+            result.Should().HaveElementAt(index, number);
+        }
+
+        result.Should().HaveCount(11);
+    }
+    
+    [Fact]
+    public void CanRunInRange()
+    {
+        //Arrange
+        List<int> result = new();
+        
+        //Act
+        foreach (var i in 10)
+        {
+            result.Add(i);
+        }
+
+        //Assert
+        for (var index = 0; index < result.Count; index++)
+        {
+            var number = index;
+            result.Should().HaveElementAt(index, number);
+        }
+
+        result.Should().HaveCount(11);
+    }
+}
