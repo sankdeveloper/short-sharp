2ad39e2bac26c62b2033dd57fb809226678067c8
Sanket Naik <sanket.naik@exxat.com>
2022-11-28 10:15:14 +0530

    Added BackgroundCronJobScheduler.cs

---
+++ ShortSharp/src/ShortSharp/Utility/BackgroundCronJobScheduler.cs
@@ -0,0 +1,54 @@
+using Timer = System.Timers.Timer;
+
+namespace ShortSharp.Utility;
+
+/// <summary>
+/// Simple, Basic, Elegant, InMemory implementation of job scheduler
+/// </summary>
+public class BackgroundCronJobScheduler
+{
+    private static BackgroundCronJobScheduler? _instance;
+    public static BackgroundCronJobScheduler Instance => _instance ??= new BackgroundCronJobScheduler();
+    public readonly IDictionary<DateTime, Timer> ScheduledTasks = new Dictionary<DateTime, Timer>();
+
+    private BackgroundCronJobScheduler()
+    {
+    }
+
+    public void ScheduleNew(Action jobFunction, int crownIntervalInMinutes)
+    {
+        //run first time when start
+        jobFunction.Invoke();
+
+        //invoke crown jobFunction after interval
+        using var timer = new Timer(TimeSpan.FromMinutes(crownIntervalInMinutes).TotalMilliseconds);
+        timer.Elapsed += (sender, eventArgs) => { jobFunction.Invoke(); };
+        timer.Start();
+
+        ScheduledTasks.Add(DateTime.UtcNow, timer);
+    }
+
+    public void Clear(DateTime dateTime)
+    {
+        if (!ScheduledTasks.ContainsKey(dateTime))
+        {
+            throw new InvalidDataException();
+        }
+        
+        var task = ScheduledTasks[dateTime];
+        task.Stop();
+        task.Close();
+        task.Dispose();
+        ScheduledTasks.Clear();
+    }
+
+    public void ClearAll()
+    {
+        foreach (var task in ScheduledTasks)
+        {
+            task.Value.Stop();
+            task.Value.Close();
+            task.Value.Dispose();
+        }
+    }
+}
