cd19092500836a226eef14d3029546dfd9b8b4eb
Sanket Naik <sanket.naik@exxat.com>
2022-12-02 13:20:05 +0530

    IEnumerable for tasks

--- ShortSharp/src/ShortSharp/Extensions/EnumerableExtension.cs
+++ ShortSharp/src/ShortSharp/Extensions/EnumerableExtension.cs
@@ -6,12 +6,12 @@ public static class EnumerableExtension
     {
         return source.Shuffle().Take(count);
     }
-    
+
     public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
     {
         return source.Reverse().OrderBy(x => Guid.NewGuid()).Reverse();
     }
-    
+
     public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, int nTimes)
     {
         var sourceTmp = Enumerable.Empty<T>();
@@ -19,16 +19,93 @@ public static class EnumerableExtension
         {
             sourceTmp = source.Shuffle();
         }
+
         return sourceTmp;
     }
-    
+
     public static T PickRandom<T>(this IEnumerable<T> source)
     {
         return source.PickRandom(1).Single();
     }
-    
-    public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> action) {
-        foreach (var item in sequence) 
+
+    public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> action)
+    {
+        foreach (var item in sequence)
             action(item);
+    }
+
+    public static string Join(this IEnumerable<string> sequence, string separator = "")
+    {
+        return string.Join(separator, sequence);
+    }
+
+    public static async Task<IEnumerable<T>> WhenAllAsync<T>(this IEnumerable<Task<T>> tasks)
+    {
+        if (tasks is null)
+            throw new ArgumentNullException(nameof(tasks));
+
+        return await Task
+            .WhenAll(tasks)
+            .ConfigureAwait(false);
+    }
+
+    public static Task WhenAllAsync(this IEnumerable<Task> tasks)
+    {
+        if (tasks is null)
+            throw new ArgumentNullException(nameof(tasks));
+
+        return Task
+            .WhenAll(tasks);
+    }
+
+    public static async Task<IEnumerable<T>> WhenAllSequentialAsync<T>(this IEnumerable<Task<T>> tasks)
+    {
+        if (tasks is null)
+            throw new ArgumentNullException(nameof(tasks));
+
+        var results = new List<T>();
+        foreach (var task in tasks)
+            results.Add(await task.ConfigureAwait(false));
+
+        return results;
+    }
+
+    public static async Task WhenAllSequentialAsync(this IEnumerable<Task> tasks)
+    {
+        if (tasks is null)
+            throw new ArgumentNullException(nameof(tasks));
+
+        foreach (var task in tasks)
+            await task.ConfigureAwait(false);
+    }
+
+    public static async Task<IEnumerable<T>> WhenAllByChunkAsync<T>(
+        this IEnumerable<Task<T>> tasks,
+        int chunkSize
+    )
+    {
+        if (tasks is null)
+            throw new ArgumentNullException(nameof(tasks));
+
+        var results = new List<T>();
+        foreach (var chunk in tasks.Chunk(chunkSize))
+        {
+            var chunkResults = await Task.WhenAll(chunk).ConfigureAwait(false);
+            results.AddRange(chunkResults);
+        }
+
+        return results;
+    }
+
+    public static async Task WhenAllByChunkAsync(
+        this IEnumerable<Task> tasks,
+        int chunkSize
+    )
+    {
+        if (tasks is null)
+            throw new ArgumentNullException(nameof(tasks));
+
+        foreach (var chunk in tasks.Chunk(chunkSize))
+            await Task.WhenAll(chunk).ConfigureAwait(false);
     }
 }
--- ShortSharp/test/Test.ShortSharp/EnumerableExtensionTest.cs
+++ ShortSharp/test/Test.ShortSharp/EnumerableExtensionTest.cs
@@ -33,6 +33,10 @@ public class EnumerableExtensionTest
         var item = _list.PickRandom(2).ToList();
         Assert.NotNull(item);
         item.Should().HaveCount(2);
+        
+        var str = _list.PickRandom();
+        Assert.NotNull(item);
+        str.Should().NotBeNull();
     }
     
     [Fact]
@@ -45,7 +49,7 @@ public class EnumerableExtensionTest
     [Fact]
     public void ShuffleNTimes()
     {
-        var item = _list.Shuffle(50).ToList();
+        var item = _list.Shuffle().Shuffle(50).ToList();
         item.Should().HaveCount(5);
     }
     
@@ -55,6 +59,69 @@ public class EnumerableExtensionTest
         _list.AsEnumerable().ForEach(item =>
         {
             item.Should().NotBeNull();
+        });
+    }
+    
+    [Fact]
+    public void JoinIEnumerableString()
+    {
+        var expected = _list.Aggregate((a, b) => a + "," + b);
+        var result = _list.Join(",");
+
+        expected.Should().Be(result);
+    }
+    
+    [Fact]
+    public async Task WhenAllAsync()
+    {
+        var asyncTasks = Enumerable.Range(0, 10)
+            .Select(number=> Task<int>.Factory.StartNew(() =>
+            {
+                Task.Delay(5000); 
+                return number;
+            }));
+        
+        var result = await asyncTasks.WhenAllAsync();
+        result.Should().HaveCount(10);
+    }
+    
+    [Fact]
+    public async Task WhenAllSequentialAsync()
+    {
+        var asyncTasks = Enumerable.Range(0, 10)
+            .Select(number=> Task<int>.Factory.StartNew(() =>
+            {
+                Task.Delay(5000); 
+                return number;
+            }));
+
+        var result = await asyncTasks.WhenAllSequentialAsync();
+
+        var existingNumber = -1;
+        result.ForEach(number =>
+        {
+            number.Should().BeGreaterThan(existingNumber);
+            existingNumber = number;
+        });
+    }
+    
+    [Fact]
+    public async Task WhenAllByChunkAsync()
+    {
+        var asyncTasks = Enumerable.Range(0, 10)
+            .Select(number=> Task<int>.Factory.StartNew(() =>
+            {
+                Task.Delay(5000); 
+                return number;
+            }));
+
+        var result = await asyncTasks.WhenAllByChunkAsync(2);
+
+        var existingNumber = -1;
+        result.ForEach(number =>
+        {
+            number.Should().BeGreaterThan(existingNumber);
+            existingNumber = number;
         });
     }
 }
